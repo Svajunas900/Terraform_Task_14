da8a3467fa816ed998c4489ca1452bd9ab45cf07
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-18 16:34:56 +0200

    Change

--- lambda/my_lambda_function.py
+++ lambda/my_lambda_function.py
@@ -1,4 +1,4 @@
-import urllib3
+import requests
 
 
 class ErrorHandler:
@@ -7,14 +7,13 @@ class ErrorHandler:
 
 
 def lambda_handler(event, context): 
-    http = urllib3.PoolManager()
     user_input = None
     if event.get("queryStringParameters"):
         user_input = event.get("queryStringParameters").get("user_input")
     else:
         user_input = event.get("user_input")
     try:
-        r = http.request('GET', 
+        r = requests.get( 
         'http://api.marketstack.com/v1/eod?' +
         'access_key=1993a5a661a25f23c46c92e886a6294b&' +
         f'symbols={user_input}')
