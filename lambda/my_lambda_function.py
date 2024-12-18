2e97e0f2a5ed21f1a292239ecdceb699a0e23fb2
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-18 16:05:52 +0200

    Modify my_lambda_function

--- lambda/my_lambda_function.py
+++ lambda/my_lambda_function.py
@@ -1,7 +1,24 @@
-import requests
+import urllib3
 
 
-def lambda_handler(event, context):   
-    response = requests.get("https://www.test.com/")
-    print(response.text)
-    return response.text 
+class ErrorHandler:
+    def error():
+        return "Something went wrong"
+
+
+def lambda_handler(event, context): 
+    http = urllib3.PoolManager()
+    user_input = None
+    if event.get("queryStringParameters"):
+        user_input = event.get("queryStringParameters").get("user_input")
+    else:
+        user_input = event.get("user_input")
+    try:
+        r = http.request('GET', 
+        'http://api.marketstack.com/v1/eod?' +
+        'access_key=1993a5a661a25f23c46c92e886a6294b&' +
+        f'symbols={user_input}')
+    except:
+        return ErrorHandler.error()
+    return r.data
+
