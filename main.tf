3cefa69f79c800ec967beead2bf0d99d7c059d14
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-18 14:52:45 +0200

    Restructure

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 
 
 resource "aws_lambda_function" "app_lambda_function"{
-  filename = "lambda.my_lambda_function.zip"
+  filename = "lambda.zip"
   function_name = "lambda_handler"
   role = aws_iam_role.iam_for_lambda.arn
   handler = "lambda.my_lambda_function.lambda_handler"
