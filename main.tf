2614b5dd15f6624e7a0363f6891ca7f27089824a
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-18 14:05:45 +0200

    Configurate lambda function creation on aws

---
+++ main.tf
@@ -0,0 +1,30 @@
+data "aws_iam_policy_document" "assume_role"{
+    statement {
+      effect = "Allow"
+
+      principals {
+        type = "Service"
+        identifiers = [ "lambda.amazonaws.com" ]
+      }
+
+      actions = ["sts:AssumeRole"]
+    }
+}
+
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "lambda-user"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+
+resource "aws_lambda_function" "lambda_function"{
+  filename = "my_lambda_function.zip"
+  function_name = "lambda_handler"
+  role = ""
+  handler = "my_lambda_function.lambda_handler"
+
+  source_code_hash = filebase64sha256("my_lambda_function.zip")
+
+  runtime = "python3.13"
+}
