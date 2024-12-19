85b07717269bb7a47e3ba157039a40fe1f0ed66b
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-19 17:05:58 +0200

    Modify main.tf

--- main.tf
+++ main.tf
@@ -30,6 +30,14 @@ resource "aws_lambda_function" "app_lambda_function"{
 }
 
 
+resource "aws_lambda_layer_version" "lambda_layer" {
+  filename   = "requests.zip"
+  layer_name = "requests"
+
+  compatible_runtimes = ["python3.13"]
+}
+
+
 resource "null_resource" "install_dependencies" {
   provisioner "local-exec" {
     command = "pip install -r lambda/requirements.txt -t lambda/"
