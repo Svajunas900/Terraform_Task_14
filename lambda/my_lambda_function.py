2ea8368760e5ccd5dfa8ab593d0f3663990df528
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-19 11:51:26 +0200

    Modify lambda_function

--- lambda/my_lambda_function.py
+++ lambda/my_lambda_function.py
@@ -12,12 +12,13 @@ def lambda_handler(event, context):
         user_input = event.get("queryStringParameters").get("user_input")
     else:
         user_input = event.get("user_input")
+    payload = {
+        'access_key': '1993a5a661a25f23c46c92e886a6294b', 
+        'symbols': {user_input}
+        }
     try:
-        r = requests.get( 
-        'http://api.marketstack.com/v1/eod?' +
-        'access_key=1993a5a661a25f23c46c92e886a6294b&' +
-        f'symbols={user_input}')
+        r = requests.get('http://api.marketstack.com/v1/eod', 
+        params=payload)
     except:
         return ErrorHandler.error()
-    return r.data
-
+    return r.json()
